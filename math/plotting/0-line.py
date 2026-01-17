35bc00e92e3e41aa798358f357bf187f7267ad7a
mahlet-tilahun <m.tilahun@alustudent.com>
2026-01-17 18:08:54 +0300

    Task 0

---
+++ math/plotting/0-line.py
@@ -0,0 +1,11 @@
+##!/usr/bin/env python3
+'''
+Plots y = x^3 as a red line graph
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+y = np.arange(0, 11) ** 3
+plt.xlim(0, 10)
+plt.plot(y, c="red")
+plt.show()
