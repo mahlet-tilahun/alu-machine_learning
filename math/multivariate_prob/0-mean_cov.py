a62ae7c6385023da67ddf2cb13a1fe6484ad5092
mahlet-tilahun <m.tilahun@alustudent.com>
2026-02-28 22:39:15 +0300

    multivariate prob

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Defines a function that calculates the mean and covariance of a data set
+"""
+
+
+import numpy as np
+
+
+def mean_cov(X):
+    """
+    Calculates the mean and covariance of a data set
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    n, d = X.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+    mean = np.mean(X, axis=0, keepdims=True)
+    cov = (1 / (n - 1)) * np.matmul(X.T - mean.T, X - mean)
+    return (mean, cov)
