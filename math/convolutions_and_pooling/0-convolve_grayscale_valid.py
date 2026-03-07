5c3f9e519e0641ef1f1775a63984cc3dde1ce95c
mahlet-tilahun <m.tilahun@alustudent.com>
2026-03-07 19:59:12 +0300

    Convolutions and pooling

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Defines a function that performs valid convolution
+on a grayscale image
+"""
+
+
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """
+    Performs a valid convolution on grayscale images
+    """
+    m = images.shape[0]
+    height = images.shape[1]
+    width = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    convoluted = np.zeros((m, height - kh + 1, width - kw + 1))
+    for h in range(height - kh + 1):
+        for w in range(width - kw + 1):
+            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel,
+                            axis=1).sum(axis=1)
+            convoluted[:, h, w] = output
+    return convoluted
